1144b9fcf662c1fb3610a6ba3e812b7a39bbf6b8
konfoton <konrad.burdach@gmail.com>
2024-06-01 10:46:03 +0200

    finall

--- pythonVSc/test.py
+++ pythonVSc/test.py
@@ -8,17 +8,19 @@ def generate(size):
 
 test1 = generate(1000)
 test2 = generate(1000)
-
-print("C time: ")
-
-a = time.time()
-cython.matc(test1, test2)
-b = time.time()
-print(b - a)
-
-print("Python time: ")
-
-a = time.time()
-matpython.matrixmul(test1, test2)
-b = time.time()
-print(b - a)
+with open("results.txt", "a") as f:
+    for x in range(100, 1300, 100):
+        test1 = generate(x)
+        test2 = generate(x)
+        f.write(f"two matrixes {x} x {x}\n")
+        f.write("C time: \n")
+        a = time.time()
+        cython.matc(test1, test2)
+        b = time.time()
+        f.write(f"time in seconds: {b-a}\n")
+        f.write("Python time: \n")
+        a = time.time()
+        matpython.matrixmul(test1, test2)
+        b = time.time()
+        f.write(f"time in seconds: {b-a}\n")
+        f.write("\n")
